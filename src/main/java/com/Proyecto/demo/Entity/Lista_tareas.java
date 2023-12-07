12ae6dd42c08b8c1fac542d441d111abb24fc091
John Cler <choquejohn6@gmail.com>
2023-12-07 05:24:13 -0400

    Agregando Entity Lista_tarea

--- src/main/java/com/Proyecto/demo/Entity/Lista_tareas.java
+++ src/main/java/com/Proyecto/demo/Entity/Lista_tareas.java
@@ -1,4 +1,37 @@
 package com.Proyecto.demo.Entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Lista_tareas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String Descripcion;
+    private  Boolean Estado;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        Estado = estado;
+    }
 }
