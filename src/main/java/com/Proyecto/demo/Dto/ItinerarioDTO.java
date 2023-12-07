1652ca030f77e8704a68b14010d5f70c97a81ef4
John Cler <choquejohn6@gmail.com>
2023-12-06 22:31:21 -0400

    Agregando Controller intinerario

---
+++ src/main/java/com/Proyecto/demo/Dto/ItinerarioDTO.java
@@ -0,0 +1,22 @@
+package com.Proyecto.demo.Dto;
+
+public class ItinerarioDTO {
+    private String topic;
+    private String plan;
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getPlan() {
+        return plan;
+    }
+
+    public void setPlan(String plan) {
+        this.plan = plan;
+    }
+}
