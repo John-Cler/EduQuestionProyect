63b0348ed25dc011de74a9c0b314ea3102c9c190
John Cler <choquejohn6@gmail.com>
2023-12-06 22:34:06 -0400

    Agegando controller intinerario

---
+++ src/main/java/com/Proyecto/demo/Controller/ItinerarioController.java
@@ -0,0 +1,25 @@
+package com.Proyecto.demo.Controller;
+
+import com.Proyecto.demo.Dto.ItinerarioDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/itinerarios")
+public class ItinerarioController {
+//    private final ItinerarioService itinerarioService;
+//
+//    @Autowired
+//    public ItinerarioController(ItinerarioService itinerarioService) {
+//        this.itinerarioService = itinerarioService;
+//    }
+//
+//    @PostMapping
+//    public ItinerarioDTO generarItinerario(@RequestBody ItinerarioDTO itinerarioDTO) {
+//        // Aquí puedes llamar al servicio para generar el itinerario y devolver el DTO resultante
+//        return itinerarioService.generarItinerario(itinerarioDTO);
+//    }
+}
