9aa7f96cb5001da48b4afe3e2c4773a92d77b71e
John Cler <choquejohn6@gmail.com>
2023-12-07 05:50:57 -0400

    Agregando ListaTareasController

--- src/main/java/com/Proyecto/demo/Controller/ListarTareaController.java
+++ src/main/java/com/Proyecto/demo/Controller/ListarTareaController.java
@@ -1,4 +1,26 @@
 package com.Proyecto.demo.Controller;
 
+import com.Proyecto.demo.Dto.ListarTareasDTO;
+import com.Proyecto.demo.Services.ListarTareaSrevice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tareas")
 public class ListarTareaController {
+    private final ListarTareaSrevice listarTareaSrevice;
+
+    @Autowired
+    public ListarTareaController(ListarTareaSrevice listarTareaSrevice) {
+        this.listarTareaSrevice = listarTareaSrevice;
+    }
+
+    @GetMapping
+    public List<ListarTareasDTO> listarTareas() {
+        return listarTareaSrevice.listarTareas();
+    }
 }
