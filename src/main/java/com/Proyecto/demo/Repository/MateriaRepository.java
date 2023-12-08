e7dc03db69eda99a848a5f56d662920976781bc5
John Cler <choquejohn6@gmail.com>
2023-12-08 11:47:12 -0400

    Agregando MateriaRepository

---
+++ src/main/java/com/Proyecto/demo/Repository/MateriaRepository.java
@@ -0,0 +1,9 @@
+package com.Proyecto.demo.Repository;
+
+import com.Proyecto.demo.Entity.Materia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MateriaRepository extends JpaRepository<Materia, Long> {
+}
