7a8fa4f3767a35f6eff7331825c3566f26509b0a
John Cler <choquejohn6@gmail.com>
2023-12-08 22:09:52 -0400

    Agregando Service y Controller

---
+++ src/main/java/com/Proyecto/demo/Controller/TarjetaController.java
@@ -0,0 +1,48 @@
+package com.Proyecto.demo.Controller;
+
+import com.Proyecto.demo.Dto.TarjetaDTO;
+import com.Proyecto.demo.Services.TarjetaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tarjetas")
+
+public class TarjetaController {
+
+    @Autowired
+    private TarjetaService tarjetaService;
+
+    @PostMapping
+    public TarjetaDTO crearTarjeta(@RequestBody TarjetaDTO tarjetaDTO) {
+        // Lógica para crear una nueva tarjeta y devolver su representación TDO
+        return tarjetaService.crearTarjeta(tarjetaDTO);
+    }
+
+    @GetMapping
+    public List<TarjetaDTO> listarTarjetas() {
+        // Lógica para listar todas las tarjetas y devolver sus representaciones TDO
+        return tarjetaService.listarTarjetas();
+    }
+
+    @GetMapping("/{id}")
+    public TarjetaDTO obtenerTarjeta(@PathVariable Long id) {
+        // Lógica para obtener una tarjeta por su ID y devolver su representación TDO
+        return tarjetaService.obtenerTarjeta(id);
+    }
+
+    @PutMapping("/{id}")
+    public TarjetaDTO actualizarTarjeta(@PathVariable Long id, @RequestBody TarjetaDTO tarjetaDTO) {
+        // Lógica para actualizar una tarjeta y devolver su representación TDO
+        return tarjetaService.actualizarTarjeta(id, tarjetaDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarTarjeta(@PathVariable Long id) {
+        // Lógica para eliminar una tarjeta por su ID
+        tarjetaService.eliminarTarjeta(id);
+    }
+
+}
