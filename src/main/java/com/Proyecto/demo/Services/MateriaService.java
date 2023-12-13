85b9ed2cbd1939617ac972785cb9cec9c932e8d2
John Cler <choquejohn6@gmail.com>
2023-12-13 00:58:27 -0400

    Modificando MateriaService

--- src/main/java/com/Proyecto/demo/Services/MateriaService.java
+++ src/main/java/com/Proyecto/demo/Services/MateriaService.java
@@ -1,40 +1,178 @@
 package com.Proyecto.demo.Services;
 
 import com.Proyecto.demo.Dto.MateriaDto;
+import com.Proyecto.demo.Entity.Materia;
+import com.Proyecto.demo.Repository.MateriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 @Service
-public class MateriaService {
-    // Lógica de negocio para las operaciones CRUD
-    // Puedes utilizar un repositorio de Spring Data JPA para acceder a la base de datos
+public class MateriaService implements MateriaRepository {
+    @Autowired
+    private MateriaRepository materiaRepository;
 
-    public MateriaDto crearMateria(MateriaDto materiaDTO) {
-        // Lógica para crear una nueva materia y devolver su representación TDO
+    @Override
+    public List<Materia> buscarPorUsuarioGrado(int usuario_grado_id) {
+        return materiaRepository.buscarPorUsuarioGrado(usuario_grado_id);
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends Materia> S saveAndFlush(S entity) {
         return null;
     }
 
-    public List<MateriaDto> listarMaterias() {
-        // Lógica para listar todas las materias y devolver sus representaciones TDO
-        // ...
+    @Override
+    public <S extends Materia> List<S> saveAllAndFlush(Iterable<S> entities) {
         return null;
     }
 
-    public MateriaDto obtenerMateria(Long id) {
-        // Lógica para obtener una materia por su ID y devolver su representación TDO
-        // ...
+    @Override
+    public void deleteAllInBatch(Iterable<Materia> entities) {
+
+    }
+
+    @Override
+    public void deleteAllByIdInBatch(Iterable<Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public Materia getOne(Long aLong) {
         return null;
     }
 
-    public MateriaDto actualizarMateria(Long id, MateriaDto materiaDTO) {
-        // Lógica para actualizar una materia y devolver su representación TDO
-        // ...
+    @Override
+    public Materia getById(Long aLong) {
+        return materiaRepository.getReferenceById(aLong);
+    }
+
+    @Override
+    public Materia getReferenceById(Long aLong) {
         return null;
     }
 
-    public void eliminarMateria(Long id) {
-        // Lógica para eliminar una materia por su ID
-        // ...
+    @Override
+    public <S extends Materia> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
     }
+
+    @Override
+    public <S extends Materia> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends Materia> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends Materia> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Materia> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends Materia> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends Materia, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
+    }
+
+    @Override
+    public <S extends Materia> S save(S entity) {
+        return materiaRepository.save(entity);
+    }
+
+    @Override
+    public <S extends Materia> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<Materia> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public List<Materia> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Materia> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(Materia entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Materia> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public List<Materia> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<Materia> findAll(Pageable pageable) {
+        return null;
+    }
+
 }
